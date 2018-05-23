75c37a0a367b388ecfb63a7c2b4853aabdc10bd9
Humpty <humpty@Humptys-iMac.local>
2018-05-23 09:32:04 -0700

    pintrest

--- collectionviewtuto/ViewController.swift
+++ collectionviewtuto/ViewController.swift
@@ -8,16 +8,85 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UICollectionViewDataSource,PinterestLayoutDelegate {
 
+    @IBOutlet weak var collectionview: UICollectionView!
+    
+    var cars:[String]=["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg","9.jpg","10.jpg"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        if let layout = collectionview.collectionViewLayout as? PinterestLayout{
+            
+            layout.delegate = self
+            
+            
+        }
+        
+        
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func collectionView(_ collectionView:UICollectionView, heightForPhotoAtIndexPath indexPath:IndexPath) -> CGFloat{
+        
+        let imagesize = UIImage(named:cars[indexPath.row])
+        
+        if let heig = imagesize?.size.height{
+            
+            
+            
+            
+            
+            return heig / 4
+        }
+        
+        return 0.0
+        
+        
+        
+    }
+
+
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cars.count
+    }
+    
+    
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "mycell", for: indexPath) as? CollectionViewCell
+        
+        cell?.collectionviewcell.image = UIImage(named: cars[indexPath.row])
+        cell?.mylabel.text = cars[indexPath.row] as? String
+        cell?.collectionviewcell.contentMode = .scaleAspectFill
+        cell?.mylabel.contentMode = .bottom
+        
+        cell?.layer.cornerRadius = 5
+        cell?.clipsToBounds = true
+        cell?.layer.masksToBounds = true
+        
+        
+        
+        return cell!
+        
+        
     }
 
 
